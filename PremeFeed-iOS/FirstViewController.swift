3540edbca4ded7de41e81920adb81970a2e7b8f2
Samuel Steele <steelesam72@gmail.com>
2015-12-20 11:20:16 -0500

    Initial Commit

---
+++ PremeFeed-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PremeFeed-iOS
+//
+//  Created by Cryptoc1 on 12/20/15.
+//  Copyright © 2015 Cryptoc1. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
